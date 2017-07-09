b460acdd7540c9412b62fbbcb90f810131b9d190
Mwarukason <a.mwaruka93@gmail.com>
2017-07-09 14:19:02 +0800

    second commit

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Table(models.Model):
+    title_one = models.CharField(max_length=100)
+    title_two = models.CharField(max_length=100)
+    title_three = models.CharField(max_length=100)
+    title_four = models.CharField(max_length=100)
+    title_five = models.CharField(max_length=100)
+    title_six = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title_one
